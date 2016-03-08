0b178ce7b6b301ac1b38de1d4436db46bfa549f3
henanzhg <854702773@qq.com>
2016-03-08 20:42:59 +0800

    Initial Commit

---
+++ SingletonPattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SingletonPattern
+//
+//  Created by mac on 3/8/16.
+//  Copyright © 2016 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SingletonPattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SingletonPattern
+//
+//  Created by mac on 3/8/16.
+//  Copyright © 2016 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
