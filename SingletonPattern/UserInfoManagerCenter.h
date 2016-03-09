9cefb20be3ca6eb5035e641cf6c764b8121714f5
henanzhg <854702773@qq.com>
2016-03-09 09:29:27 +0800

    first commit

---
+++ SingletonPattern/UserInfoManagerCenter.h
@@ -0,0 +1,13 @@
+//
+//  UserInfoManagerCenter.h
+//  SingletonPattern
+//
+//  Created by mac on 3/8/16.
+//  Copyright © 2016 mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserInfoManagerCenter : NSObject
+
+@end
---
+++ SingletonPattern/UserInfoManagerCenter.m
@@ -0,0 +1,13 @@
+//
+//  UserInfoManagerCenter.m
+//  SingletonPattern
+//
+//  Created by mac on 3/8/16.
+//  Copyright © 2016 mac. All rights reserved.
+//
+
+#import "UserInfoManagerCenter.h"
+
+@implementation UserInfoManagerCenter
+
+@end
